b5a119aea9b2770c41a861bbe586f7e96834c829
Tania <113130993+Tania66@users.noreply.github.com>
2023-08-25 00:04:33 +0300

    add first hw

---
+++ src/js/01-gallery.js
@@ -0,0 +1,29 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+import { galleryItems } from './gallery-items';
+
+
+console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+
+const galleryItemsList = createGalleryItems(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend' , galleryItemsList);
+
+
+function createGalleryItems (items){
+
+    return items.map(({preview, original, description}) => {
+   return ` <li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`
+}).join("");
+}
+
+
+const lightbox = new SimpleLightbox('.gallery a', { captionData: 'alt',  captionDelay: 250});
