76cb6fbcec99516a78d5f55e663072f33dbfa506
Tania <113130993+Tania66@users.noreply.github.com>
2023-08-27 20:17:55 +0300

    add 2 hw

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+
+
+const iframe = document.querySelector('iframe');
+
+const player = new Player(iframe);
+
+
+ const onPlay = function (data) {
+ localStorage.setItem("videoplayer-current-time", data.seconds);
+}
+
+const throttledOnPlay = throttle(onPlay, 1000);
+
+player.on('timeupdate', throttledOnPlay);
+
+
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
 
