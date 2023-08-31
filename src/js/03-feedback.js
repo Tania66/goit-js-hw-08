9845e1c26bbc9fc9474360e31e5b9835795ecdb0
Tania <113130993+Tania66@users.noreply.github.com>
2023-08-31 22:14:52 +0300

    add hw-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,60 @@
+import throttle from 'lodash.throttle';
+
+const form = document.querySelector(".feedback-form");
+const textarea = document.querySelector("textarea");
+const input = document.querySelector("input");
+const LOCALSTORAGE_KEY = "feedback-form-state";
+const formData = {};
+populateMessageOutput();
+
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input' , throttle(onItputForm, 500));
+
+
+
+
+
+function onItputForm(e){
+
+ formData[e.target.name] = e.target.value;
+localStorage.setItem(LOCALSTORAGE_KEY,JSON.stringify(formData));
+}
+
+
+
+
+
+
+
+function populateMessageOutput() {
+
+const saveMassege = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+
+ if (saveMassege) {
+
+console.log(saveMassege);
+textarea.value = saveMassege.message;
+input.value = saveMassege.email;
+  }
+   
+
+}
+
+
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+event.currentTarget.reset();
+localStorage.removeItem(LOCALSTORAGE_KEY);
+console.log(formData);
+}
+
+
+
+
+
+
+
 
